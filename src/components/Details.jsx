804c53162fecdf35b8daa9adeaa42017d4894b01
Nagarjuna <nagarjunajogi08@gmail.com>
2026-05-06 23:01:33 +0530

    added hero and details

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import { MapPin, UserRound, Plane } from "lucide-react";
+import "../styles/Details.css";
 
-const Details = () => {
+function Details() {
   return (
-    <div>
-      
+    <div className="details">
+      <div className="details-container">
+
+        {/* ITEM 1 */}
+        <div className="details-item">
+          <div className="icon-circle">
+            <MapPin size={28} />
+          </div>
+
+          <div className="details-content">
+            <h3>Locate Clinic</h3>
+            <button>Visit Now →</button>
+          </div>
+        </div>
+
+        {/* ITEM 2 */}
+        <div className="details-item border">
+          <div className="icon-circle">
+            <UserRound size={28} />
+          </div>
+
+          <div className="details-content">
+            <h3>Find A Dentist</h3>
+            <button>Dental Experts →</button>
+          </div>
+        </div>
+
+        {/* ITEM 3 */}
+        <div className="details-item border">
+          <div className="icon-circle">
+            <Plane size={28} />
+          </div>
+
+          <div className="details-content">
+            <h3>Explore Services</h3>
+            <button>Book now →</button>
+          </div>
+        </div>
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default Details
+export default Details;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
 
-const Hero = () => {
+import "swiper/css";
+import "../styles/Hero.css";
+
+function Hero() {
   return (
-    <div>
-      
+    <div className="hero">
+      <Swiper
+        modules={[Autoplay]}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        className="hero-swiper"
+      >
+        <SwiperSlide>
+          <img src="/slide1.png" alt="slide1" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img src="/slide2.jpeg" alt="slide2" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img src="/slide3.jpeg" alt="slide3" />
+        </SwiperSlide>
+      </Swiper>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
